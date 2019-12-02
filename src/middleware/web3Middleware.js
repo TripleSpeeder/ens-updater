3c0ed0383f93d872dade3a0c88ddbcbf3f0e3d14
Michael Bauer <michael@m-bauer.org>
2019-12-02 15:00:35 +0100

    make option "registryAddress" CamelCase

--- src/middleware/web3Middleware.js
+++ src/middleware/web3Middleware.js
@@ -4,8 +4,11 @@ const createWeb3 = async ({verbose, provider}) => {
     verbose && console.log('Setting up web3...')
     try {
         let web3 = new Web3(provider)
+        // CHECK HERE - If provider is a HDWalletProvider initialized With an invalid connectionstring this will immediately exit ??? Possibly web3 bug?
         let chainId = await web3.eth.getChainId()
-        let netId = await web3.eth.net.getId()
+        // let chainId
+        // let netId = await web3.eth.net.getId()
+        let netId
         if (verbose) {
             console.log(`\tRunning chain ID ${chainId} on network ${netId}`)
         }

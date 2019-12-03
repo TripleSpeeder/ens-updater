2de5bd45560c802196b3c63660dd21887ca91a92
Michael Bauer <michael@m-bauer.org>
2019-12-03 10:12:32 +0100

    Show chainID and netID in verbose mode again

--- src/middleware/web3Middleware.js
+++ src/middleware/web3Middleware.js
@@ -4,11 +4,8 @@ const createWeb3 = async ({verbose, provider}) => {
     verbose && console.log('Setting up web3...')
     try {
         let web3 = new Web3(provider)
-        // CHECK HERE - If provider is a HDWalletProvider initialized With an invalid connectionstring this will immediately exit ??? Possibly web3 bug?
         let chainId = await web3.eth.getChainId()
-        // let chainId
-        // let netId = await web3.eth.net.getId()
-        let netId
+        let netId = await web3.eth.net.getId()
         if (verbose) {
             console.log(`\tRunning chain ID ${chainId} on network ${netId}`)
         }

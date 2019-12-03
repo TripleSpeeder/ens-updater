76f6e872a8b12ea6525225a7cb80afd2d6813000
Michael Bauer <michael@m-bauer.org>
2019-12-03 12:40:51 +0100

    Throw if providerstring is undefined.

--- src/middleware/web3Middleware.js
+++ src/middleware/web3Middleware.js
@@ -2,13 +2,18 @@ const Web3 = require('web3')
 
 const createWeb3 = async ({verbose, provider}) => {
     verbose && console.log('Setting up web3...')
+
+    if (provider === undefined) {
+        throw Error(`Failed to initialize web3. provider is undefined.`)
+    }
+
     try {
         let web3 = new Web3(provider)
         return {
             web3,
         }
     } catch (error) {
-        throw Error(`Failed to initialize web3 at ${provider}: ${error.message}` )
+        throw Error(`Failed to initialize web3 at ${provider}: ${error.message}`)
     }
 }
 

e44d6524453cce30fe77b1006e4274f903a6a20e
Michael Bauer <michael@m-bauer.org>
2019-12-04 09:37:58 +0100

    Improve end2end test structure

--- test/end2end/errorconditions.test.js
+++ test/end2end/errorconditions.test.js
@@ -5,12 +5,22 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
-const controllerAccountIndex = 1
 const {private_keys, mnemonic} = require('./testdata')
 
+function runCommand(command, options={}) {
+    let childResult
+    try {
+        childResult = execa.commandSync(command, options)
+    } catch(childResultError) {
+        childResult = childResultError
+    }
+    return childResult
+}
 
 contract('errorConditions', function(accounts) {
 
+    const controllerAccountIndex = 1
+    const private_key = private_keys[controllerAccountIndex]
     const scriptpath = 'bin/ens-updater.js'
     const providerstring = 'http://localhost:8545'
     let registryAddress
@@ -20,58 +30,48 @@ contract('errorConditions', function(accounts) {
         registryAddress = registry.address
     })
 
-    it("Should show usage info when no commands are specified", async function() {
-        try {
-            await execa.node(scriptpath)
-        } catch(childResultError) {
-            assert.match(childResultError.stderr, /Usage: ens-updater.js <command> \[options]/)
-        }
+    it("Should show usage info when no commands are specified", function() {
+        const childResult = runCommand(scriptpath)
+        assert.isTrue(childResult.failed, "Command should have failed")
+        assert.match(childResult.stderr, /Usage: ens-updater.js <command> \[options]/)
     })
 
-    it("Should show error message when web3 connectionstring is invalid and no account required", async function() {
-        try {
-            await execa.node(scriptpath, ['getContenthash', 'wayne.test', '--web3', 'http://in.val.id:12345'])
-        } catch(childResultError) {
-            assert.match(childResultError.stderr, /Invalid JSON RPC response/)  // Error message when using plain connectionstring
-        }
+    it("Should show error message when web3 connectionstring is invalid and no account required", function() {
+        const command = `${scriptpath} getContenthash wayne.test --web3 http://in.val.id:12345`
+        const childResult = runCommand(command)
+        assert.isTrue(childResult.failed, "Command should have failed")
+        assert.match(childResult.stderr, /Invalid JSON RPC response/)  // Error message when using plain connectionstring
     })
 
-    it("Should show error message when web3 connectionstring is invalid and account is required", async function() {
-        try {
-            await execa.node(
-                scriptpath, ['setAddress', 'wayne.test', '0x123', '--web3', 'http://in.valid:12345'],
-                {
-                    env: {
-                        PRIVATE_KEY: private_keys[controllerAccountIndex]
-                    }
-                }
-            )
-        } catch (childResultError) {
-            assert.match(childResultError.stderr, /Failed to initialize web3/)  // Error message when using HDWalletProvider
+    it("Should show error message when web3 connectionstring is invalid and account is required", function() {
+        const command = `${scriptpath} setAddress wayne.test 0x123 --web3 http://in.valid:12345`
+        const options = {
+            env: { PRIVATE_KEY: private_key }
         }
+        const childResult = runCommand(command, options)
+        assert.isTrue(childResult.failed, "Command should have failed")
+        assert.match(childResult.stderr, /Failed to initialize web3/)  // Error message when using HDWalletProvider
     })
 
     it("Should show error message when account is required but no credentials are provided", async function() {
         const targetAddress = accounts[3]
-        try {
-            await execa.node(scriptpath, ['setAddress', 'wayne.test', targetAddress, '--web3', providerstring])
-        } catch (childResultError){
-            assert.match(childResultError.stderr, /Got neither mnemonic nor private key/)
-        }
+        const command = `${scriptpath} setAddress wayne.test ${targetAddress} --web3 ${providerstring}`
+        const childResult = runCommand(command)
+        assert.isTrue(childResult.failed, "Command should have failed")
+        assert.match(childResult.stderr, /Got neither mnemonic nor private key/)
     })
 
     it("Should show error message when account is required and both mnemonic and private key are provided", async function() {
         const targetAddress = accounts[3]
-        try {
-            await execa.node(scriptpath, ['setAddress', 'wayne.test', targetAddress, '--web3', providerstring],
-                {
-                    env: {
-                        MNEMONIC: mnemonic,
-                        PRIVATE_KEY: private_keys[controllerAccountIndex]
-                    }
-                })
-        } catch (childResultError){
-            assert.match(childResultError.stderr, /Got both mnemonic and private key/)
+        const command = `${scriptpath} setAddress wayne.test ${targetAddress} --web3 ${providerstring}`
+        const options = {
+            env: {
+                MNEMONIC: mnemonic,
+                PRIVATE_KEY: private_key
+            }
         }
+        const childResult = runCommand(command, options)
+        assert.isTrue(childResult.failed, "Command should have failed")
+        assert.match(childResult.stderr, /Got both mnemonic and private key/)
     })
 })
